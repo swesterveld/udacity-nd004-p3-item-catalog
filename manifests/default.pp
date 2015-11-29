6b916aff0dd2e3a12b92be04ac865ed6ef6b2b64
Sil Westerveld <github@silwesterveld.com>
2015-11-29 22:34:37 +0100

    Modify dependencies between resources and improve some of them.

--- manifests/default.pp
+++ manifests/default.pp
@@ -32,13 +32,9 @@ package { $pypi_packages:
 class { 'postgresql::server': }
 
 postgresql::server::db { 'beercatalog':
-    #user => 'vagrant',
-    #password => postgresql_password('vagrant', 'vagrant'),
-    #grant => all,
     user     => 'connoisseur',
-    #password => 'michaeljackson',
     password => postgresql_password('connoisseur', 'michaeljackson'),
-    require => Package['postgresql'],
+    before => Exec['load fixtures'],
 }
 
 postgresql::server::pg_hba_rule { 'allow localhost to access beercatalog database':
@@ -48,6 +44,7 @@ postgresql::server::pg_hba_rule { 'allow localhost to access beercatalog databas
     user => 'connoisseur',
     address => 'localhost',
     auth_method => 'trust',
+    before => Exec['load fixtures'],
 }
 
 postgresql::server::pg_hba_rule { 'allow vagrant to access beercatalog database':
@@ -57,11 +54,8 @@ postgresql::server::pg_hba_rule { 'allow vagrant to access beercatalog database'
     user => 'vagrant',
     address => 'localhost',
     auth_method => 'trust',
+    before => Exec['load fixtures'],
 }
-
-#postgresql::server::role { 'connoisseur':
-#    password_hash => postgresql('connoisseur', 'michaeljackson'),
-#}
 
 postgresql::server::role { 'vagrant': }
 
@@ -69,18 +63,18 @@ exec { 'load fixtures':
     command => 'python database_init.py',
     cwd => '/vagrant/catalog/',
     logoutput => 'true',
-    path => ['/usr/bin/'],
+    path => ['/usr/local/bin', '/usr/bin/', '/bin/'],
     user => 'vagrant',
-    #onlyif => 'psql --list | grep beercatalog',
-    unless => '',
-    subscribe => [Postgresql::Server::Db['beercatalog'], Postgresql::Server::Pg_hba_rule['allow localhost to access beercatalog database']],
+    onlyif => 'psql --list | grep beercatalog',
 }
 
 exec { 'start web application':
-    command => 'python catalog.py &',
+    command => 'python /vagrant/catalog/catalog.py &',
     cwd => '/vagrant/catalog/',
     logoutput => 'true',
-    path => ['/usr/bin/'],
+    path => ['/usr/bin/', '/bin/'],
+    unless  => 'pgrep -lu vagrant | grep python',
     user => 'vagrant',
+#    refreshonly => true,
     require => Exec['load fixtures'],
 }
